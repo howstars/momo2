9e924792f5fb92e211c53d53ec15a538d8850ce6
howstars <98864606+howstars@users.noreply.github.com>
2023-07-31 00:30:49 +0800

    Update MMSLS.js
    7.31

--- MMSLS.js
+++ MMSLS.js
@@ -20,9 +20,9 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=29615168&pid=538069c17f688fd8b5a740af4e240112&tid=f8ee7d6164801f4672eb4227e9728852",
-    "https://www.maimemo.com/share/page?uid=29615168&pid=538069c17f688fd8b5a740af4e240112&tid=f8ee7d6164801f4672eb4227e9728852",
-    "https://www.maimemo.com/share/page?uid=29615168&pid=538069c17f688fd8b5a740af4e240112&tid=f8ee7d6164801f4672eb4227e9728852",
+    "https://www.maimemo.com/share/page?uid=29615168&pid=6dc9bf4ffde7a017a5c8659c5f17929e&tid=17109773712409dcf3a31b7c4ab044fb",
+    "https://www.maimemo.com/share/page?uid=29615168&pid=89b9d20c4cb4eddd45fa3385d19b87c7&tid=5f6eba20b37d677b7c08939dda757856",
+    "https://www.maimemo.com/share/page?uid=29615168&pid=08dbd8062db6d4d8bab0cd74454a633d&tid=063d5f3931f85da2a98f044be53f519d",
   ]
   /**
    * 生成随机数字
